f7837ba5b954cf6690aecb3fcd0f52ab92e86cb9
haraldger <harald.gerhardsen@hotmail.com>
2022-12-19 11:32:41 -0500

    Fix epsilon value

--- Swin/mario.py
+++ Swin/mario.py
@@ -39,7 +39,7 @@ def mario():
     # Init data structures
     q_network = get_model()
     target_network = get_model()
-    epsilon_scheduler = EpsilonScheduler(decay_frames=DECAY_FRAMES, decay_mode=DECAY_MODE, decay_rate=DECAY_RATE, start_frames=DECAY_START_FRAMES, initial_epsilon=0)
+    epsilon_scheduler = EpsilonScheduler(decay_frames=DECAY_FRAMES, decay_mode=DECAY_MODE, decay_rate=DECAY_RATE, start_frames=DECAY_START_FRAMES)
     replay_buffer = ReplayBuffer(capacity=REPLAY_MEMORY)
     agent = SwinAgent(q_network, target_network, epsilon_scheduler, replay_buffer, num_actions=NUM_ACTIONS,
                         initial_exploration=INITIAL_EXPLORATION, sync_frequency=SYNC_FREQUENCY)
